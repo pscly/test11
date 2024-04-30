6097cbbb197c9a93a1916ff30abf5656bb9a9a18
pscly <pscly@outlook.com>
2024-04-30 11:54:58 +0800

    dict > public

--- main3.py
+++ main3.py
@@ -2,7 +2,7 @@ import time
 from pathlib import Path
 
 if __name__ == '__main__':
-    dist = Path('./dist')
-    dist.mkdir(exist_ok=True, parents=True)
-    with open(dist / 'log.txt', 'a') as f:
+    public = Path('./public')
+    public.mkdir(exist_ok=True, parents=True)
+    with open(public / 'log.txt', 'a') as f:
         a = time.strftime("%Y-%m-%d %H:%M:%S__main3", time.localtime())
