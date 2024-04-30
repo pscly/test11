83a08c38cce2e4a989dea0514870007e76715cdb
pscly <pscly@outlook.com>
2024-04-30 11:43:44 +0800

    更新main3

--- main3.py
+++ main3.py
@@ -1,5 +1,8 @@
 import time
+from pathlib import Path
 
 if __name__ == '__main__':
-    with open('log.txt', 'a') as f:
+    dist = Path('./dist')
+    dist.mkdir(exist_ok=True, parents=True)
+    with open(dist / 'log.txt', 'a') as f:
         a = time.strftime("%Y-%m-%d %H:%M:%S__main3", time.localtime())
